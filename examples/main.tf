edc5c43a825a1ef802c978925471bc4c6c25123c
Philipp Pixel <boris.schaa@cloudogu.com>
2021-03-18 12:13:27 +0100

    #3 Update example script, extract magic numbers into local constants

--- examples/main.tf
+++ examples/main.tf
@@ -23,6 +23,31 @@ provider "redmine" {
 }
 */
 
+locals {
+  redmine_default_issue_tracker_ids = {
+    bug = 1
+    user_story = 2
+    support = 3
+  }
+
+  redmine_default_issue_priorities_ids = {
+    low = 1
+    normal = 2
+    high = 3
+    urgent = 4
+    immediate = 5
+  }
+
+  redmine_default_issue_status_ids = {
+    new = 1
+    in_progress = 2
+    resolved = 3
+    feedback = 4
+    closed = 5
+    rejected = 6
+  }
+}
+
 resource "redmine_project" "project1" {
   identifier = "exampleproject"
   name = "example project"
@@ -34,7 +59,8 @@ resource "redmine_project" "project1" {
 
 resource "redmine_issue" "issue1" {
   project_id = redmine_project.project1.id
-  tracker_id = 1
+  tracker_id = local.redmine_default_issue_tracker_ids.user_story
   subject = "Something should be done"
   description = "In this ticket an **important task** should be done!\n\nGo ahead!\n\n```bash\necho -n $PATH\n```"
+  priority_id = local.redmine_default_issue_priorities_ids.immediate
 }
